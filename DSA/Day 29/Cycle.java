16b44603c1550dcc53d3b432fdf88021a260edf0
siddhantgedekar <sapwoodg18@gmail.com>
2025-06-23 18:40:39 +0530

    Merge sort in linked list.

--- DSA/Day 29/Cycle.java
+++ DSA/Day 29/Cycle.java
@@ -303,13 +303,77 @@ public class Cycle {
         prev.next = null;
     }
 
+    public Node getMid (Node head) {
+        // slow fast approach
+        Node slow = head;
+        Node fast = head.next;
+        // LOOP
+        while(fast != null && fast.next != null){
+            slow = slow.next; // +1
+            fast = fast.next.next; // +2
+        }
+        return slow; // return mid
+    }
+
+    // merge the list
+    public Node merge (Node head1, Node head2) {
+        Node mergeLL = new Node(-1);
+        Node temp = mergeLL;
+
+        while(head1 != null && head2 != null) {
+            if(head1.data <= head2.data){
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            }
+            else{
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+
+        while(head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+
+        while(head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return mergeLL.next;
+    }
+
+    // Merge sort in linked list
+    public Node mergeSort (Node head) {
+        if(head == null || head.next == null){ 
+            return head;
+        }
+        // find mid
+        Node mid = getMid(head);
+
+        // compare left and right
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+
+        // merge
+        return merge(newLeft, newRight);
+    }
+
     public static void main(String[] args) {
         Cycle ll = new Cycle();
         
         // add elements
-        ll.addFirst(3);
-        ll.addFirst(2);
         ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
         // connect like this 
         tail.next = head.next;
         // OR connect like this
@@ -318,6 +382,9 @@ public class Cycle {
         // ll.print();
         ll.removeCycle();
         System.out.println("Cycle exists: " + ll.isCycle());
+        ll.print();
+        // merge sort
+        ll.head = ll.mergeSort(ll.head);
         ll.print();
     }
 }
